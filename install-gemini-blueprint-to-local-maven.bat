4e15db389b44f697c59525533510c2a87e95b051
eis <eis@iki.fi>
2012-12-24 12:20:55 +0200

    fixed pom.xml, updated install script

--- install-gemini-blueprint-to-local-maven.bat
+++ install-gemini-blueprint-to-local-maven.bat
@@ -1,11 +1,44 @@
-@echo off
+REM @echo off
 REM if a suitable repo would be found, this step could be done easier to use that directly
 REM see instructions at http://stackoverflow.com/questions/4157170/maven-command-to-install-remote-dependency-locally
 
-set GEMINI_VERSION=1.0.2
+set GEMINI_VERSION=1.0.2.RELEASE
 
-mvn install:install-file -Dfile=gemini-blueprint-core-%GEMINI_VERSION%.RELEASE.jar -DgroupId=org.eclipse.gemini -DartifactId=org.eclipse.gemini.blueprint.core -Dversion=%GEMINI_VERSION%.RELEASE -Dpackaging=jar
+set ARTIFACTFILENAME=org.eclipse.gemini.blueprint.core-%GEMINI_VERSION%.jar
+wget "http://ebr.springsource.com/repository/app/bundle/version/download?name=org.eclipse.gemini.blueprint.core&version=%GEMINI_VERSION%&type=binary" -O "%ARTIFACTFILENAME%"
+if exist "%ARTIFACTFILENAME%" (
+  set POMFILEPATH=META-INF/maven/org.eclipse.gemini.blueprint/gemini-blueprint-core/pom.xml
+  jar xf org.eclipse.gemini.blueprint.core-%GEMINI_VERSION%.jar %POMFILEPATH%
+  call mvn install:install-file "-Dfile=%ARTIFACTFILENAME%" "-DpomFile=%POMFILEPATH%"
+  del "%ARTIFACTFILENAME%"
+  rd /s /q META-INF
+) else (
+  echo Couldn't download %ARTIFACTFILENAME%, wget missing?
+  echo ...
+)
 
-mvn install:install-file -Dfile=gemini-blueprint-io-%GEMINI_VERSION%.RELEASE.jar -DgroupId=org.eclipse.gemini -DartifactId=org.eclipse.gemini.blueprint.io -Dversion=%GEMINI_VERSION%.RELEASE -Dpackaging=jar
+set ARTIFACTFILENAME=org.eclipse.gemini.blueprint.io-%GEMINI_VERSION%.jar
+wget "http://ebr.springsource.com/repository/app/bundle/version/download?name=org.eclipse.gemini.blueprint.io&version=%GEMINI_VERSION%.RELEASE&type=binary" -O %ARTIFACTFILENAME%
+if exist "%ARTIFACTFILENAME%" (
+  set POMFILEPATH=META-INF/maven/org.eclipse.gemini.blueprint/gemini-blueprint-io/pom.xml
+  jar xf "%ARTIFACTFILENAME%" %POMFILEPATH%
+  call mvn install:install-file "-Dfile=%ARTIFACTFILENAME%" "-DpomFile=%POMFILEPATH%"
+  del "%ARTIFACTFILENAME%"
+  rd /s /q META-INF
+) else (
+  echo Couldn't download %ARTIFACTFILENAME%, wget missing?
+  echo ...
+)
 
-mvn install:install-file -Dfile=gemini-blueprint-extender-%GEMINI_VERSION%.RELEASE.jar -DgroupId=org.eclipse.gemini -DartifactId=org.eclipse.gemini.blueprint.extender -Dversion=%GEMINI_VERSION%.RELEASE -Dpackaging=jar
+set ARTIFACTFILENAME=org.eclipse.gemini.blueprint.extender-%GEMINI_VERSION%.jar
+wget "http://ebr.springsource.com/repository/app/bundle/version/download?name=org.eclipse.gemini.blueprint.extender&version=%GEMINI_VERSION%.RELEASE&type=binary" -O "%ARTIFACTFILENAME%"
+if exist "%ARTIFACTFILENAME%" (
+  set POMFILEPATH=META-INF/maven/org.eclipse.gemini.blueprint/gemini-blueprint-extender/pom.xml
+  jar xf "%ARTIFACTFILENAME%" %POMFILEPATH%
+  call mvn install:install-file "-Dfile=%ARTIFACTFILENAME%" "-DpomFile=%POMFILEPATH%"
+  del "%ARTIFACTFILENAME%"
+  rd /s /q META-INF
+) else (
+  echo Couldn't download %ARTIFACTFILENAME%, wget missing?
+  echo ...
+)
