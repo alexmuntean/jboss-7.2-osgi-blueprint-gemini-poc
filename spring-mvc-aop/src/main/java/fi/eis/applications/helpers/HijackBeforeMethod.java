6b107bc349214d5cddb7eb8355ad5f1e4f5a0828
eis <eis@iki.fi>
2013-01-08 11:00:31 +0200

    we're using aspectj, not spring aop

--- spring-mvc-aop/src/main/java/fi/eis/applications/helpers/HijackBeforeMethod.java
+++ spring-mvc-aop/src/main/java/fi/eis/applications/helpers/HijackBeforeMethod.java
@@ -1,22 +1,14 @@
 package fi.eis.applications.helpers;
 
-import java.lang.reflect.Method;
-
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.springframework.aop.MethodBeforeAdvice;
 
 @Aspect
-public class HijackBeforeMethod implements MethodBeforeAdvice {
-	@Override
-	public void before(Method arg0, Object[] arg1, Object arg2)
-			throws Throwable {
-		System.out.println("HijackBeforeMethod : Before method hijacked! " + arg0.getName());
-	}
+public class HijackBeforeMethod {
+
 	@Before("execution(public * fi.eis.applications.beans.FooClass*.*(..))")
     public void printSomething(JoinPoint jp) {
 		 System.out.println("HijackBeforeMethod : Before execution of method :: " +jp.getSignature().getName());
 	}
-
 }
