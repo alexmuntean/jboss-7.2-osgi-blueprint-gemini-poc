b53c2c814b5ae0d0d85a9166ed2af1d0beb8a16d
eis <eis@iki.fi>
2013-02-18 19:36:35 +0200

    added some convenience stuff

--- spring-mvc/src/main/java/fi/eis/applications/controllers/HomeController.java
+++ spring-mvc/src/main/java/fi/eis/applications/controllers/HomeController.java
@@ -1,8 +1,11 @@
 package fi.eis.applications.controllers;
 
 import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.Enumeration;
+import java.util.List;
 import java.util.Locale;
 
 import javax.servlet.ServletContext;
@@ -11,13 +14,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.osgi.web.context.support.OsgiBundleXmlWebApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.context.ServletContextAware;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.InvalidSyntaxException;
+import org.osgi.framework.ServiceReference;
 
 import fi.eis.applications.beans.FooInterface;
 import fi.eis.applications.jboss.poc.compositeservice.gemini.api.InformationService;
@@ -28,12 +35,19 @@ import fi.eis.applications.jboss.poc.osgiservice.api.MessageService;
  */
 @Controller
 @Lazy
-public class HomeController {
+public class HomeController implements ServletContextAware {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	private FooInterface foo;
 	private InformationService informationService;
+	
+	private BundleContext bundleContext;
+
+	public void setServletContext(ServletContext context) {
+		this.bundleContext =
+			(BundleContext) context.getAttribute(OsgiBundleXmlWebApplicationContext.BUNDLE_CONTEXT_ATTRIBUTE);
+	}	
 	
 	//@Autowired
 	public HomeController(InformationService informationServiceParam,
@@ -59,7 +73,33 @@ public class HomeController {
 		model.addAttribute("messageFromLocalBean", (foo != null ? foo.getMessage() : "empty"));
 		model.addAttribute("messageFromOSGIService", (informationService != null ? informationService.getMessage() : "empty"));
 
+		model.addAttribute("osgibundles", getBundleList(bundleContext));
+		model.addAttribute("osgiservices", getServiceList(bundleContext));		
+		
 		return "home";
 	}
-	
+	private static List<String> getServiceList(BundleContext context) {
+		List<String> services = new ArrayList<String>();
+		try {
+			for (ServiceReference ref : context.getAllServiceReferences(null,
+					null)) {
+				services.add(context.getService(ref).toString());
+				context.ungetService(ref);
+			}
+		} catch (InvalidSyntaxException e) {
+			throw new IllegalStateException(e);
+		}
+		Collections.sort(services);
+		return services;
+	}
+	private static List<String> getBundleList(BundleContext context) {
+		List<String> bundles = new ArrayList<String>();
+		for (Bundle bundle : context.getBundles()) {
+			bundles.add(bundle.getSymbolicName() + ":"
+					+ bundle.getVersion().toString());
+		}
+		
+		Collections.sort(bundles);
+		return bundles;
+	}
 }
--- spring-mvc/src/main/webapp/WEB-INF/views/home.jsp
+++ spring-mvc/src/main/webapp/WEB-INF/views/home.jsp
@@ -9,8 +9,21 @@
 	Hello world!  
 </h1>
 
-<P>  The time on the server is ${serverTime}. </P>
-<P>  The word from locals is ${messageFromLocalBean}. </P>
-<P>  The word from other homies is ${messageFromOSGIService}. </P>
+<p>  The time on the server is ${serverTime}. </p>
+<p>  The word from locals is ${messageFromLocalBean}. </p>
+<p>  The word from other homies is ${messageFromOSGIService}. </p>
+
+<h3>OSGi bundles found</h3>
+<ul>
+<c:forEach items="${osgibundles}" var="item">
+   <li>${item}</li>
+</c:forEach>
+</ul>
+<h3>OSGi services found</h3>
+<ul>
+<c:forEach items="${osgiservices}" var="item">
+   <li>${item}</li>
+</c:forEach>
+</ul>
 </body>
 </html>
